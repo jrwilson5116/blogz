b4f908ed49dc461caf42b67512e72583152de865
jrwilson5116 <jrwilson5116@gmail.com>
2018-03-26 17:59:14 -0500

    adds code from build-a-blog as a foundation

---
+++ main.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, redirect, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy 
+
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:bab@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Blog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(30))
+    body = db.Column(db.String(2000))
+
+    def __init__(self, name,body):
+        self.title = name
+        self.body =body
+
+@app.route('/')
+def index():
+    return redirect(url_for('blog'))
+
+@app.route('/blog', methods=['GET'])
+def blog():
+    blog_post = request.args.get('id')
+
+    if blog_post is None:
+        tasks = Blog.query.all()
+    else:
+        tasks = Blog.query.filter_by(id = blog_post)
+    return render_template('blog.html',title="Blog", tasks=tasks)
+
+
+@app.route('/newpost',methods=['POST','GET'])
+def add_post():
+    if request.method == 'POST':
+        blog_name = request.form['title']
+        blog_body = request.form['body']
+        new_blog = Blog(blog_name,blog_body)
+        db.session.add(new_blog)
+        db.session.commit()
+        return redirect(url_for('blog'))
+    return render_template('newpost.html')
+
+if __name__=='__main__':
+    app.run()

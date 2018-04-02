f25168389128d677ebe010d1ad88efb31df77841
jrwilson5116 <jrwilson5116@gmail.com>
2018-04-02 18:27:25 -0500

    adds working signup feature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy 
-
+from input_checks import valid_length, match
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -35,6 +35,7 @@ class User(db.Model):
 
 @app.route('/')
 def index():
+    #TODO add home page
     return redirect(url_for('blog'))
 
 
@@ -62,14 +63,42 @@ def add_post():
 
 @app.route('/signup',methods=['POST','GET'])
 def signup():
-    return render_template('signup.html')
+    name_error = ''
+    password_error = ''
+    match_error = ''
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify_password = request.form['verify']
+
+        if not valid_length(username):
+            name_error = 'Invalid username'
+        if not valid_length(password):
+            password_error = 'Invalid password'
+        if not match(password,verify_password):
+            match_error = 'Passwords do not match'
+        
+        if valid_length(username) and valid_length(password) and match:
+            new_user = User(username,password)
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for('add_post'))
+
+    return render_template('signup.html').format(name_error,password_error,match_error)
 
 @app.route('/login', methods=['POST','GET'])
 def login():
+    #TODO process login attempt
+    # if username is real and password matches store username in session and redirect to newpost page
+    # if username does not exist, redirect to login with bad username message
+    # if username exists but password does not match
+
     return render_template('login.html')
 
 @app.route('/logout')
 def logout():
+    #TODO remove username from session
     return redirect(url_for('blog'))
 
 
